95e3291cd89de345815bebb13d66f24b148543d0
KASIREDDYASMITHA <asmithareddy2022@gmail.com>
2025-12-20 23:16:49 +0530

    all files

---
+++ Props/code/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Component1 from "./components/Component1";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Props Drilling Example</h2>
+      <Component1 />
+    </div>
+  );
+}
+
+export default App;
